f8cd1ced19535be6cb8490cf539da5ef32061a96
DubstepDragon <reynoldsjt2.4@gmail.com>
2025-01-17 10:57:53 -0500

    updating files to remove to preserve backups.

--- Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
+++ Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
@@ -1,42 +1,28 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using AWBOI.AMP.Core.Abstracts;
+using AWBOI.AMP.DLC;
 using UnityEditor;
 using AWBOI.AMP.Merger;
 using UnityEngine;
 
 namespace AWBOI.Editor
 {
-    
+
     class AwboiPostProcessor : AssetPostprocessor
     {
-        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPaths)
         {
             if (AwboiMerger.Instance)
             {
                 AwboiMerger.Instance.SetDlcRefresh();
-                UpdateNamespaces();
             }
-        }
-        
-        public static void UpdateNamespaces()
-        {
-            string[] guids = AssetDatabase.FindAssets("t:DLCOptions");
-            foreach (string guid in guids)
-            {
-                string path = AssetDatabase.GUIDToAssetPath(guid);
-                string fileContent = File.ReadAllText(path);
-
-                // Update the namespace in the script
-                if (fileContent.Contains("namespace OldNamespace"))
-                {
-                    fileContent = fileContent.Replace("namespace AWBOI", "namespace AWBOI.AMP.Core.Utils");
-                    File.WriteAllText(path, fileContent);
-
-                    // Refresh the asset database to apply changes
-                    AssetDatabase.ImportAsset(path);
-                }
-            }
-
-            Debug.Log("[AwboiPostProcessor] DLCOptions Updated");
         }
     }
+
+
 }
