98d4b01435a2895585a871c7174b062c51a91402
DubstepDragon <reynoldsjt2.4@gmail.com>
2025-01-16 22:45:23 -0500

    updating post processor to check for updating files

--- Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
+++ Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using UnityEditor;
 using AWBOI.AMP.Merger;
+using UnityEngine;
 
 namespace AWBOI.Editor
 {
@@ -11,7 +13,35 @@ namespace AWBOI.Editor
             if (AwboiMerger.Instance)
             {
                 AwboiMerger.Instance.SetDlcRefresh();
+                bool checkedForFileUpdate = EditorPrefs.GetBool("AwboiPostProcessor_CheckedForFileUpdates", false);
+                if (!checkedForFileUpdate)
+                {
+                    UpdateNamespaces();
+                    EditorPrefs.SetBool("AwboiPostProcessor_CheckedForFileUpdates", true);
+                }
             }
+        }
+        
+        public static void UpdateNamespaces()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:DLCOptions");
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                string fileContent = File.ReadAllText(path);
+
+                // Update the namespace in the script
+                if (fileContent.Contains("namespace OldNamespace"))
+                {
+                    fileContent = fileContent.Replace("namespace AWBOI", "namespace AWBOI.AMP.Core.Utils");
+                    File.WriteAllText(path, fileContent);
+
+                    // Refresh the asset database to apply changes
+                    AssetDatabase.ImportAsset(path);
+                }
+            }
+
+            Debug.Log("[AwboiPostProcessor] DLCOptions Updated");
         }
     }
 }
