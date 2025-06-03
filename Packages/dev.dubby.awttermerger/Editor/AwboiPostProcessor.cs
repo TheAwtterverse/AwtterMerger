e7f18b910968ec0de49b0131038e6b4bcbe878bd
DubstepDragon <reynoldsjt2.4@gmail.com>
2025-06-03 13:50:14 -0400

    Awboi Backup Manager Ready for Main

--- Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
+++ Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
@@ -1,5 +1,7 @@
 #if UNITY_EDITOR
+using AWBOI.AMP.Core.DLC;
 using AWBOI.AMP.Merger;
+using AWBOI.AMP.Unmerger;
 using UnityEditor;
 
 namespace Editor
@@ -10,10 +12,9 @@ namespace Editor
         static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
             string[] movedFromAssetPaths)
         {
-            if (AwboiMerger.Instance)
-            {
-                AwboiMerger.Instance.SetDlcRefresh();
-            }
+            if (AwboiMerger.Instance) AwboiMerger.Instance.SetDlcRefresh();
+            
+            if (BackupManagerWindow.Instance) BackupManagerWindow.Instance.ResetVars();
         }
     }
 
