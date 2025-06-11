7cb686f2976eb9ded01eba49d91dc360d96801da
DubstepDragon <reynoldsjt2.4@gmail.com>
2025-06-11 16:32:57 -0400

    New DLC maker working

--- Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
+++ Packages/dev.dubby.awttermerger/Editor/AwboiPostProcessor.cs
@@ -1,5 +1,12 @@
 #if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AWBOI.AMP.Core.Abstracts;
 using AWBOI.AMP.Core.DLC;
+using AWBOI.AMP.DLC;
 using AWBOI.AMP.Merger;
 using AWBOI.AMP.Unmerger;
 using UnityEditor;
@@ -12,10 +19,21 @@ namespace Editor
         static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
             string[] movedFromAssetPaths)
         {
-            if (AwboiMerger.Instance) AwboiMerger.Instance.SetDlcRefresh();
-            
-            if (BackupManagerWindow.Instance) BackupManagerWindow.Instance.ResetVars();
+            ResetClasses(AwboiMerger.Instance);
+            ResetClasses(AwboiUnmerger.Instance);
+            ResetClasses(NewDlcMakerWindow.Instance);
+            ResetClasses(BackupManagerWindow.Instance);
         }
+
+        static void ResetClasses<T>(T window) where T : BaseMerger
+        {
+            if (window && EditorWindow.HasOpenInstances<T>())
+            {
+                window.RefreshVars();
+                window.RefreshAllUI();
+            }
+        }
+        
     }
 
 
